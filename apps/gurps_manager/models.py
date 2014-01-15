b3424d96c960e97b881c33cc1619e1d74f2fbd35
Jeremy Audet <ichimonji10@gmail.com>
2014-01-15 11:58:51 -0500

    Add extremely simple database model.
    Model contains only two classes: "Character" and "Race".

---
+++ apps/gurps_manager/models.py
@@ -0,0 +1,42 @@
+"""Database schema for GURPS Manager.
+
+It is possible to generate a diagram of the schema defined herein. See the
+readme for details.
+
+If a model does not specify a primary key, django automatically generates a
+column named ``id``. Django will not generate ``id`` if you pass ``primary_key =
+True`` to some other column.
+
+"""
+from django.db import models
+
+# pylint: disable=R0903
+# "Too few public methods (0/2)"
+# It is both common and OK for a model to have no methods.
+#
+# pylint: disable=W0232
+# "Class has no __init__ method"
+# It is both common and OK for a model to have no __init__ method.
+
+class Character(models.Model):
+    """An individual who can be role-played."""
+    MAX_LEN_NAME = 50
+    MAX_LEN_DESCRIPTION = 2000
+
+    name = models.CharField(max_length = MAX_LEN_NAME)
+    description = models.TextField(
+        max_length = MAX_LEN_DESCRIPTION,
+        blank = True
+    )
+    race = models.ForeignKey('Race')
+
+class Race(models.Model):
+    """A race to which a character can belong."""
+    MAX_LEN_NAME = 50
+    MAX_LEN_DESCRIPTION = 2000
+
+    name = models.CharField(max_length = MAX_LEN_NAME)
+    description = models.TextField(
+        max_length = MAX_LEN_DESCRIPTION,
+        blank = True
+    )
