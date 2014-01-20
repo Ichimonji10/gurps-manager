9916620916c0b82996fb1210f02d5ab2c5fd200e
Henry Corse <kaedraar@gmail.com>
2014-01-20 09:49:17 -0500

    added fields to character model in models.py

--- apps/gurps_manager/models.py
+++ apps/gurps_manager/models.py
@@ -22,9 +22,41 @@ class Character(models.Model):
     """An individual who can be role-played."""
     MAX_LEN_NAME = 50
     MAX_LEN_DESCRIPTION = 2000
+    MAX_LEN_STORY = 2000
 
     name = models.CharField(max_length = MAX_LEN_NAME)
     description = models.TextField(
         max_length = MAX_LEN_DESCRIPTION,
         blank = True
     )
+    story = models.CharField(
+        max_length = MAX_LEN_STORY,
+        blank = True
+    )
+    """If possible, this should be limited to 2 decimal places"""
+    total_points = models.FloatField() 
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    intelligence = models.IntegerField()
+    health = models.IntegerField()
+    appearance = models.IntegerField()
+    wealth = models.IntegerField()
+    magery = models.IntegerField()
+    eidetic_memory = models.IntegerField()
+    used_fatigue = models.FloatField()
+    bonus_fatigue = models.IntegerField()
+    bonus_hitpoints = models.IntegerField()
+    bonus_alertness = models.IntegerField()
+    bonus_willpower = models.IntegerField()
+    bonus_fright = models.IntegerField()
+    bonus_speed = models.IntegerField()
+    bonus_movement = models.IntegerField()
+    bonus_dodge = models.IntegerField()
+    bonus_initiative = models.IntegerField()
+    free_strength = models.IntegerField()
+    free_dexterity = models.IntegerField()
+    free_intelligence = models.IntegerField()
+    free_health = models.IntegerField()
+    
+    
+    
