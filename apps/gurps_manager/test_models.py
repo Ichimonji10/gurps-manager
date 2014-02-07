cd55053283a3da13adfb5f1e45a7deb91d7253f2
Jeremy Audet <ichimonji10@gmail.com>
2014-02-06 22:09:03 -0500

    Add module `test_models`.
    The responsibility of this new module is to test all virtual model attributes.
    As is, the new module only tests a single method on a single class. However,
    this small amount of code provides a template for writing many future tests.

---
+++ apps/gurps_manager/test_models.py
@@ -0,0 +1,24 @@
+"""Unit tests for the ``models`` module.
+
+Each test case in this module tests a single model. For example, the
+``CampaignTestCase`` tests just the ``Campaign`` model.
+
+"""
+from django.test import TestCase
+from gurps_manager import factories
+
+# pylint: disable=E1101
+# E: 16,19: Class 'CampaignFactory' has no 'build' member (no-member)
+# Pylint does not detect the build and create methods provided by factory_boy.
+#
+# pylint: disable=R0904
+# R: 11, 0: Too many public methods (72/20) (too-many-public-methods)
+# All classes inheriting from TestCase will cause this warning.
+
+class CampaignTestCase(TestCase):
+    """Tests for ``Campaign``."""
+    def test_str(self):
+        """Test the ``__str__`` method."""
+        name = factories.campaign_name()
+        campaign = factories.CampaignFactory.build(name=name)
+        self.assertEqual(name, str(campaign))
