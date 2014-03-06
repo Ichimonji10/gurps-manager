296cd2828b6b830ee408866e81c0956c1a95c7fa
Jeremy Audet <ichimonji10@gmail.com>
2014-03-05 20:14:29 -0500

    Create a form for logging in a user

--- apps/gurps_manager/forms.py
+++ apps/gurps_manager/forms.py
@@ -4,7 +4,7 @@ Unless otherwise noted, all forms defined herein can be used to either create or
 update an object.
 
 """
-from django.forms import ModelForm
+from django.forms import CharField, Form, ModelForm, widgets
 from gurps_manager import models
 
 # pylint: disable=R0903
@@ -33,3 +33,12 @@ class CharacterForm(ModelForm):
         # There are a lot of attributes on the Character model, and we want to
         # display them all. If we want to display only some fields, use `fields`
         # or `exclude`.
+
+class LoginForm(Form):
+    """A form for logging in a user."""
+    username = CharField()
+    password = CharField(widget=widgets.PasswordInput)
+
+    class Meta(object):
+        """Form attributes that are not fields."""
+        fields = ['username', 'password']
