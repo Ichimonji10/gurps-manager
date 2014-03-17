b892f2fe09c51d694c5cfbb47424c3a6d7d67439
Henry Corse <kaedraar@gmail.com>
2014-03-17 18:21:15 -0400

    fixed the URLs for the trait and hit locations pages to actually point to the correct view.

--- apps/gurps_manager/urls.py
+++ apps/gurps_manager/urls.py
@@ -160,22 +160,22 @@ urlpatterns = patterns( # pylint: disable=C0103
     ),
     url(
         r'^character/(\d+)/traits/$',
-        login_required(views.Possessions.as_view()),
+        login_required(views.Traits.as_view()),
         name='gurps-manager-character-id-traits',
     ),
     url(
         r'^character/(\d+)/traits/update-form/$',
-        login_required(views.PossessionsUpdateForm.as_view()),
+        login_required(views.TraitsUpdateForm.as_view()),
         name='gurps-manager-character-id-traits-update-form',
     ),
     url(
         r'^character/(\d+)/hit-locations/$',
-        login_required(views.Possessions.as_view()),
+        login_required(views.HitLocations.as_view()),
         name='gurps-manager-character-id-hit-locations',
     ),
     url(
         r'^character/(\d+)/hit-locations/update-form/$',
-        login_required(views.PossessionsUpdateForm.as_view()),
+        login_required(views.HitLocationsUpdateForm.as_view()),
         name='gurps-manager-character-id-hit-locations-update-form',
     ),
 )
