7858d99c1ea0f69af9be88fade23e9b0b65f51fe
Jeremy Audet <ichimonji10@gmail.com>
2014-04-15 21:47:50 -0400

    Polish module `make_user`.
    Make the help text more helpful, reformat code, trim down excessive whitespace
    and commenting, etc.

--- apps/gurps_manager/management/commands/make_user.py
+++ apps/gurps_manager/management/commands/make_user.py
@@ -10,27 +10,25 @@ from optparse import make_option
 
 class Command(BaseCommand):
     """Defines how to register the ``make_user`` command with ``manage.py``."""
-    args = '<username><password>'
+    args = '<username> <password>'
     help = 'Create a user object for the application.'
-
     option_list = BaseCommand.option_list + (
-        make_option('--admin',
+        make_option(
+            '--admin',
             action='store_true',
             dest='admin',
             default=False,
-            help='make the user an admin'),
-        )
+            help='make the user an admin'
+        ),
+    )
 
     def handle(self, *args, **options):
-        """Create a User with the given username and password"""
-        # Grab the username and password out of the args list
+        """Create a User object."""
         username = args[0]
         password = args[1]
         email = ''
-        # Create the user object
-        user = User.objects.create_user(username, email, password)
 
-        # Make the user an admin if the flag is set
+        user = User.objects.create_user(username, email, password)
         if options['admin']:
             user.is_staff = True
             user.is_superuser = True
